5cfaeae0c28d943eef1e102a3fc0f3d86485a00f
Mihir Srivastava <55758677+mihir1919@users.noreply.github.com>
2023-06-10 20:03:25 +0530

    adding

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling when clicking on navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
